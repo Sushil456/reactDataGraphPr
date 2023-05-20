5ed6de1426e8de295b39df370a5fb7e77f4b513b
Sushil456 <amisushil469@gmail.com>
2023-05-21 01:13:36 +0530

    background color

--- src/reactSelf.jsx
+++ src/reactSelf.jsx
@@ -4,12 +4,16 @@ export const Reactself =()=>{
 
   
   const students = [
-    {id:1, firstname:'sushil', age:23, country:'india'},
-    {id:2, firstname:'Rahul', age:23, country:'Mumbai'},
-    {id:3, firstname:'Jyoti', age:23, country:'bangaldesh'},
-    {id:4, firstname:'Kaalia', age:23, country:'UK'},
+    {id:1, firstname:'sushil', age:23, country:'india',active:true},
+    {id:2, firstname:'Rahul', age:23, country:'Mumbai',active:false},
+    {id:3, firstname:'Jyoti', age:23, country:'bangaldesh',active:false},
+    {id:4, firstname:'Kaalia', age:23, country:'UK',active:null},
   ]
   
+
+  
+
+
   const [selectedStudent, setSelectedStudent] = useState(null);
 
   const showDetails = (student) => {
@@ -20,6 +24,23 @@ export const Reactself =()=>{
     setSelectedStudent(null);
   };
 
+  const getCardBorderColor = (active) => {
+    let backgroundColor;
+    let borderColor;
+
+    if (active === null) {
+      backgroundColor = '#f2f2f2';
+      borderColor = 'black';
+    } else if (active) {
+      backgroundColor = '#e6f2e6';
+      borderColor = 'green';
+    } else {
+      backgroundColor = '#ffe6e6';
+      borderColor = 'red';
+    }
+
+    return { backgroundColor, borderColor };
+  };
   
 
 
@@ -53,6 +74,7 @@ export const Reactself =()=>{
       <div className="container">
       <h1>Student Details</h1>
       <table className="table table-hover">
+
         <thead>
           <tr>
             <th>#</th>
@@ -61,9 +83,16 @@ export const Reactself =()=>{
             <th>Country</th>
           </tr>
         </thead>
+
         <tbody>
-          {students.map((student) => (
-            <tr key={student.id} onClick={() => showDetails(student)} data-bs-toggle="tooltip" data-bs-placement="right" title="Click to view details">
+        {students.map((student) => (
+            <tr
+              key={student.id}
+              onClick={() => showDetails(student)}
+              data-bs-toggle="tooltip"
+              data-bs-placement="right"
+              title="Click to view details"
+            >
               <th scope="row">{student.id}</th>
               <td>{student.firstname}</td>
               <td>{student.age}</td>
@@ -74,7 +103,7 @@ export const Reactself =()=>{
       </table>
 
       {selectedStudent && (
-        <div className="card floating-card">
+        <div className="card" style={getCardBorderColor(selectedStudent.active)}>
           <div className="card-body">
             <h5 className="card-title">{selectedStudent.firstname}</h5>
             <h6 className="card-subtitle mb-2 text-muted">Age: {selectedStudent.age}</h6>
